999d8b6df16d001393d37a81c63a9b229ed1ea4e
devshift-stack <ds@activi.io>
2025-12-16 19:54:22 +0100

    Initial shared package

---
+++ lib/kids_ai_shared.dart
@@ -0,0 +1,7 @@
+/// Shared library for Kids AI educational apps
+library kids_ai_shared;
+
+// Export shared components here
+// export 'src/widgets/common_button.dart';
+// export 'src/services/api_service.dart';
+// export 'src/models/user.dart';
